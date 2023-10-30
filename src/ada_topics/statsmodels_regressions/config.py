a0e34743d3021650fd24ee1bc609ca34c3c656b8
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2023-10-30 14:04:36 +0100

    Statsmodels instead of pandas statistics.

---
+++ src/ada_topics/statsmodels_regressions/config.py
@@ -0,0 +1,28 @@
+"""Definitions of source files for the current chapter."""
+
+import itertools
+
+from ada_topics.pandas_data.what_is_pandas import SITE_CONTENTS as WHAT_IS_PANDAS
+
+TOPICS = [
+    WHAT_IS_PANDAS,
+]
+SITE_CONTENTS = {
+    "chapter_title": "Regressions with Statsmodels",
+    "pages": tuple(
+        itertools.chain(
+            ("content_objectives.md",),
+            *[topic["pages"] for topic in TOPICS],
+        ),
+    ),
+    "other": tuple(
+        itertools.chain(
+            *[topic["other"] for topic in TOPICS],
+        ),
+    ),
+    "built": tuple(
+        itertools.chain(
+            *[topic["built"] for topic in TOPICS],
+        ),
+    ),
+}
