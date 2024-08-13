974d7037c1390ca18cf9781d82f507198172f266
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2024-08-13 15:21:24 +0200

    Remove chapter-level objectives/contents.

--- src/ada_topics/python_basics/config.py
+++ src/ada_topics/python_basics/config.py
@@ -26,7 +26,6 @@ SITE_CONTENTS = {
     "chapter_title": "Python Basics",
     "pages": tuple(
         itertools.chain(
-            ("content_objectives.md",),
             *[topic["pages"] for topic in TOPICS],
         ),
     ),
