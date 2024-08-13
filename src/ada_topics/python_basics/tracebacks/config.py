056f2eb3d81ebbc799e27728a1a8b6cde4228e22
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2024-08-13 10:40:35 +0200

    Clean up stuff that was copied from epp_topics but unused.

--- src/ada_topics/python_basics/tracebacks/config.py
+++
@@ -1,7 +0,0 @@
-"""Definitions of source files for the current chapter."""
-
-SITE_CONTENTS = {
-    "pages": ("objectives_materials.ipynb",),
-    "other": ("screencast/public/simple_traceback.png",),
-    "built": ("python_basics-tracebacks.pdf",),
-}
